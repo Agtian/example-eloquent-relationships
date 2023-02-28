6de7d9358ef5a437b4b97fb251b44c7621467b0a
Agtian Muhamad Ricky Tanshidiq <agtianrickey33@gmail.com>
2023-02-28 22:53:53 +0700

    add one to one and one to many

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 
 Route::get('/users', [UserController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
